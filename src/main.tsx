6d7413399aeefbd5dde9ede7549ddd7ad9cfd39a
aiwon0035 <OsakoSeiho@gmail.com>
2024-03-25 13:55:13 +0900

    一時的にstrict modeをコメントアウト

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //useEffectが2回実行されcardが2回作られるので、前のcardと新しいcardが同じか判別できないため一旦消した
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
